e00925b903da86af2f347d48347504514995cc6d
Josh Gustafson <j@gustafson.me>
2026-08-29 17:41:17 -0600

    Migrate dependency selections to pin.nix

--- pin.nix
+++ pin.nix
@@ -1,4 +1,13 @@
 {
   version = "0.9.92";
   hash = "sha256-Td+tjCoi073Eg+fZcgCcjW+Mi4bIdIyNwHfFsZTBDjo=";
+  dependencies = {
+    "comfyui-workflow-templates-core" = "v0.3.246";
+    "comfyui-workflow-templates-json" = "v0.1.61";
+    "comfyui-workflow-templates-media-api" = "v0.3.79";
+    "comfyui-workflow-templates-media-video" = "v0.3.91";
+    "comfyui-workflow-templates-media-image" = "v0.3.146";
+    "comfyui-workflow-templates-media-other" = "v0.3.212";
+    "comfyui-workflow-templates-media-assets-01" = "v0.1.36";
+  };
 }
